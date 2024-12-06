b05fd6e647d3c82452c3360d4176c9fd594e0169
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-12-06 07:47:59 +0700

    feat: feat some route and set up mutler upload

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,12 +1,22 @@
-const express = require('express');
-const multer = require('multer');
-const upload = multer()
-const UserController = require('../controller/userController') 
-const authMiddleware = require('../middleware/authMiddleware')
-const router = express.Router()
+const express = require("express");
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+const upload = multer({ storage: storage })
+
+const UserController = require("../controller/userController");
+const authMiddleware = require("../middleware/authMiddleware");
+const router = express.Router();
 
 const userController = new UserController();
-router.get('/profile', upload.none(), userController.profile)
-router.put('/update', upload.none(), authMiddleware, userController.update)
+router.get("/profile", upload.none(), userController.profile);
+router.put("/profile/update", upload.none(), authMiddleware, userController.update);
+router.post("/profile/image", upload.single("file"), authMiddleware, userController.picture);
 
 module.exports = router;
